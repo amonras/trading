d4ce9ae3bd76a1f5aaed9a676159f516e544098f
Alex Monras <amb.physis@gmail.com>
2021-12-05 19:25:32 +0100

    Add unit tests

--- backtesting/tests/test_backtesting.py
+++ backtesting/tests/test_backtesting.py
@@ -1,8 +1,41 @@
-import unittest
+import datetime
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+import pytest
 
-if __name__ == '__main__':
-    unittest.main()
+from strategies.ichimoku import Ichimoku
+from strategies.obv import Obv
+from strategies.psar import Psar
+from strategies.sma import Sma
+from strategies.strategy import Strategy
+from strategies.support_resistance import SupportResistance
+
+
+@pytest.fixture(
+    params=[
+        Obv(ma_period=10),
+        Ichimoku(20, 10),
+        SupportResistance(2, 2, 1, 1, 1),
+        Sma(20, 10),
+        Psar(.1, .1, .1)
+    ]
+)
+def strategy(request) -> Strategy:
+    strtgy = request.param
+    yield strtgy
+    if hasattr(strtgy, 'db_client'):
+        strtgy.db_client.hf.close()
+
+
+def test_strategy(strategy: Strategy):
+    exchange = 'binance'
+    symbol = 'BTCUSDT'
+
+    tf = '15m'
+    from_time = 0
+    to_time = int(datetime.datetime.now().timestamp() * 1000)
+
+    strategy.set_target(exchange=exchange, symbol=symbol, tf=tf, from_time=from_time, to_time=to_time, path="tests/resources")
+    ret = strategy.backtest()
+
+    assert isinstance(ret, tuple)
+    assert len(ret) == 2
