05a95643705498ba20033e4ac8bc52570e6049e3
Alex Monras <amb.physis@gmail.com>
2021-12-05 21:13:12 +0100

    Optimizer unit testing

--- backtesting/tests/test_optimizer.py
+++ backtesting/tests/test_optimizer.py
@@ -2,36 +2,69 @@ import datetime
 
 import pytest
 
+from models import BacktestResult
+from optimization.optimizer import Nsga2
 from strategies import *
 from strategies.strategy import Strategy
+
+exchange = 'binance'
+symbol = 'BTCUSDT'
+
+tf = '15m'
+from_time = 0
+to_time = int(datetime.datetime.now().timestamp() * 1000)
+
+POP_SIZE = 10
 
 
 @pytest.fixture(
     params=[
-        Obv(ma_period=10),
-        Ichimoku(20, 10),
-        SupportResistance(2, 2, 1, 1, 1),
-        Sma(20, 10),
-        Psar(.1, .1, .1)
+        'obv',
+        'ichimoku',
+        'sup_res',
+        'sma',
+        'psar'
     ]
 )
-def strategy(request) -> Strategy:
+def strategy(request) -> str:
     strtgy = request.param
     yield strtgy
-    if hasattr(strtgy, 'db_client'):
-        strtgy.db_client.hf.close()
 
 
-def test_strategy(strategy: Strategy):
-    exchange = 'binance'
-    symbol = 'BTCUSDT'
+def test_initial_population(strategy: str):
+    nsga2 = Nsga2(exchange, symbol, strategy, tf, from_time, to_time, POP_SIZE)
 
-    tf = '15m'
-    from_time = 0
-    to_time = int(datetime.datetime.now().timestamp() * 1000)
+    assert len(nsga2.create_initial_population()) == POP_SIZE
 
-    strategy.set_target(exchange=exchange, symbol=symbol, tf=tf, from_time=from_time, to_time=to_time, path="tests/resources")
-    ret = strategy.backtest()
 
-    assert isinstance(ret, tuple)
-    assert len(ret) == 2
+def test_create_new_population():
+    pass
+
+
+def test_crowding_distance():
+    pass
+
+
+def test_create_offspring_population():
+    pass
+
+
+def test_non_dominated_sorting():
+    pass
+
+
+def test_evaluate_population():
+    population = {}
+    nsga2 = Nsga2(exchange, symbol, 'obv', tf, from_time, to_time, POP_SIZE)
+
+    for pnl in range(10):
+        for max_dd in range(10):
+            br = BacktestResult()
+            br.pnl = pnl
+            br.max_dd = max_dd
+            population[pnl*100 + max_dd] = br
+
+    fronts = nsga2.non_dominated_sorting(population)
+
+    # There should be 19 fronts as there are 19 anti-diagonals in a 10x10 matrix
+    assert len(fronts) == 19
