37f6652d4543f533b9323bef652c2c1c2c90e150
Alex Monras <amb.physis@gmail.com>
2021-12-05 15:19:03 +0100

    Add C++ code

---
+++ backtesting/backtestingCpp/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+
+#include "Database.h"
+#include "Utils.h"
+#include "strategies/Psar.h"
+#include "Utils.h"
+
+
+int main(int, char**) {
+    // Database db("binance");
+    // int array_size = 0;
+    // double** res = db.get_data("BTCUSDT", "binance", array_size);
+    
+    // std::vector<double> ts, open, high, low, close, volume;
+    // std::tie(ts, open, high, low, close, volume) = rearrange_candles(res, "5m", 1609344600000, 1609354600000, array_size);
+    // for (int i =0; i < 100; i++) {
+    //     printf("%f %f %f %f %f %f\n", ts[i], open[i], high[i], low[i], close[i], volume[i]);
+    // }
+    // printf("%lu\n", ts.size());
+    // db.close_file();
+
+    std::string symbol = "BTCUSDT";
+    std::string exchange = "binance";
+    std::string timeframe = "30m";
+
+    char* symbol_char =  strcpy((char*)malloc(symbol.length() + 1), symbol.c_str());
+    char* exchange_char = strcpy((char*)malloc(exchange.length() + 1), exchange.c_str());
+    char* tf_char = strcpy((char*)malloc(timeframe.length() + 1), timeframe.c_str());
+
+    Psar psar(exchange_char, symbol_char, tf_char, 1609344600000.0, 1609354200000.0);
+    psar.execute_backtest(0.02, 0.02, 0.2);
+    printf("%f | %f\n", psar.pnl, psar.max_dd);
+}
